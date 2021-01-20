4ea0cc5b3dd6b4d6aa96df23a5c9923fd0cf92a7
yangseungmi <0105627@gmail.com>
2021-01-21 01:24:23 +0900

    Update Quiz4963.java

--- Problem solving/BackJoon/Quiz4963.java
+++ Problem solving/BackJoon/Quiz4963.java
@@ -1,7 +1,6 @@
 // 2021 1 19
 
 // 섬의 개수(난이도 중)
-
 package com.company.armicpc;
 
 import java.io.*;
@@ -10,31 +9,68 @@ import java.util.StringTokenizer;
 public class Quiz4963 {
     private static int w, h;
     private static int[][] map;
-    private static boolean[][] visited;
+    private static boolean[][] isCheck;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = null;
 
-        while (true){
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
             st = new StringTokenizer(br.readLine());
 
             w = Integer.parseInt(st.nextToken());
             h = Integer.parseInt(st.nextToken());
-            if(w == 0 && h == 0) break;
-            
-            map = new int[h][w];
-            visited = new boolean[h][w];
 
-            for (int i = 0; i < h; i++) {
+            // 0 0 일떄 빠져나옴
+            if (w == 0 && h == 0) break;
+
+            map = new int[h + 2][w + 2]; // 계산시 주위 인덱스 검사할때 오류생길수 있으니
+            isCheck = new boolean[h + 2][w + 2];
+
+            for (int i = 1; i <= h; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < w; j++) {
+                for (int j = 1; j <= w; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-        }
 
-        System.out.println("a"+map[0][0]);
+            int num = 0;
+            // 섬을 세보자.
+            for (int i = 1; i <= h; i++) {
+                for (int j = 1; j <= w; j++) {
+                    if (map[i][j] == 1 && !isCheck[i][j]) {
+                        isCheck[i][j] = true;
+                        getIsland(i, j);
+                        num++;
+                    }
+                }
+            }
+            sb.append(num).append("\n");
+        }
+        System.out.println(sb.toString());
+        br.close();
     }
+
+    static void getIsland(int w, int h) {
+
+        map[h][w] = 0;
+        // 대상을 둘러싸는 인덱스
+        // ex) 대상 [1,1] : 주위 [0,0] [0,1] [0,2] / [1,0] [1,2] / [2,0] [2,1] [2,2]
+        int[] search_w = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] search_h = {-1, 0, 1, -1, 1, -1, -1, 1};
+
+        for (int i = 0; i < search_h.length; i++) {
+            int around_x = w + search_w[i];
+            int around_y = h + search_h[i];
+
+            if (map[around_y][around_x] == 1) {
+                isCheck[around_y][around_x] = true;
+                getIsland(around_x, around_y);
+            }
+        }
+    }
+
 }
+
