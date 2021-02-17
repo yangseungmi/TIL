245ccc0ccba6a8ab5f186371fa8ddc9fbccc315f
yangseungmi <0105627@gmail.com>
2021-02-18 01:36:42 +0900

    Update Quiz11286.java

--- Problem solving/BackJoon/Quiz11286.java
+++ Problem solving/BackJoon/Quiz11286.java
@@ -20,7 +20,7 @@ public class Quiz11286 {
         int n = Integer.parseInt(br.readLine());
 
         PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
-            int abs1 = Math.abs(o1);
+            int abs1 = Math.abs(o1); // 절대값 구하는 Method
             int abs2 = Math.abs(o2);
 
             if(abs1 == abs2) return o1 > o2 ? 1 : -1;
