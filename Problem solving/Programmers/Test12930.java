c57c6e2ea317b5c12b872f4b3c2ba389ad1897f0
yangseungmi <0105627@gmail.com>
2021-03-30 00:45:41 +0900

    Update Test12930.java

--- Problem solving/Programmers/Test12930.java
+++ Problem solving/Programmers/Test12930.java
@@ -6,18 +6,23 @@
 
 class Solution {
     public String solution(String s) {
+        // "AbC dEf"로 들어왔을 때
+        // index로 카운트를 세준다
         int index = 0;
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < s.length(); i++) {
+            // 공백이면 공백을 append해주고 index를 0으로 초기화
             if(s.charAt(i) == ' ') {
                 sb.append(' ');
                 index = 0;
             } else {
                 if(index % 2 != 0) {
+                    // 홀수는 소문자
                     sb.append(Character.toLowerCase(s.charAt(i)));
                     index ++;
                 } else {
+                    // 짝수는 대문자
                     sb.append(Character.toUpperCase(s.charAt(i)));
                     index ++;
                 }
